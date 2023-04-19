0483afcd13175c6d5f0ec134e51bd390abeb3aa8
Yesid Briceño <yesidizq@hotmail.com>
2023-04-19 10:08:25 -0500

    Se agrega test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const bodyParser = require('body-parser');
-const HTTP_PORT = 8014;
+const HTTP_PORT = 8015;
 const app = express();
 const qr = require('./qr.js');
 
@@ -8,8 +8,10 @@ const qr = require('./qr.js');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.disable("x-powered-by");
 
 app.use(bodyParser.json());
+
 app.get("/api/v1/health", async (req, res, next) => {
     res.json({
         "message": "Ok",
@@ -21,10 +23,17 @@ app.get("/api/v1/health", async (req, res, next) => {
 app.post("/api/v1/qr", async (req, res, next) => {
     const body = req.body;
     const url = body.url;
-    const QrCode = await qr(url);
-    res.json({
-        QrCode
-    })
+    let QrCode = await qr(url);
+    if (QrCode == null || QrCode == undefined || QrCode == 'Error') {
+        res.status(500).json({
+            "message": "Error"
+        })
+    } else {
+        res.status(200).json({
+            "message": "Ok",
+            "QrCode": QrCode
+        })
+    }
 });
 
 app.listen(HTTP_PORT, () => {
--- qr.js
+++ qr.js
@@ -1,14 +1,16 @@
 const QRCode = require('qrcode');
 
 const generateQR = async text => {
+
+    
     let qr = '';
     try {
         qr = await QRCode.toDataURL(text);
-        qr = qr.substring(22)
+        qr = qr.substring(22);
         return qr;
     } catch (err) {
-        console.error(err)
-        return err;
+        console.error('mi error', err);
+        return 'Error';
     }
 }
 
